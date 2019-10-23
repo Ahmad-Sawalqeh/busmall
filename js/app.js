1ef737df7ef32f2362793b4bd22e060e3795ef5e
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-10-23 23:52:07 +0300

    Adding some styleing

--- js/app.js
+++ js/app.js
@@ -1,76 +1,5 @@
 'use strict';
 
-/*
-  Practice domain modeling by planning out an app w that allows users to choose their favorite between two Items
-  Let students participate by suggesting the steps needed to make the app run
-  Continue until students have provided enough detail that they feel confident they could build the app themselves
-
-  App Flow:
-  - Welcome and instructions
-  - Randomly put 2 Items on the screen
-    - Random number generator
-    - a function to display Items
-  - A user clicks on a Item
-    - event listener needs to be on the image to fire the event handler
-    - the event handler fires
-      - ? check if total clicks is 25 ?
-        - stop letting the user click
-        - display the clicks
-      -? If not ?
-        - track which Item was clicked on
-        - update clicke img count of how many times it has been clicked on
-        - update both img'count of times shown
-        - Randomly Pick 2 Items, run the same code that put them on the screen to begin with
-
-  HTML
-    - have a container for left and right img
-    - Give the img id's so we can select them
-    - let the user know what they are supposed to do
-      - instructions
-
-  More javascript details
-  We need Objects with all the image properties
-  var Image = function ()
-  {
-    name : 'cool Item',
-    clicks: 0,
-    times shown: 0,
-    url : 'cool-Item.jpg'
-  }
-
-  We need an Array to hold all image Objects
-
-  function to randomly pick an image{
-    Prevent last picked Items from being picked
-      - STRETCH pick all Items evenly as possible
-    Math.floor  Math.random() * array.length()
-    make sure left and right image are unique
-  }
-
-  click on an image, targetted by id
-  add event listener('click', function(){
-    keep track of the image that is clicked
-    prevent all currently displayed img from being re clicked {
-    }
-  })
-
-  function to display all the clicks at the end () {
-    generate a table or list
-    populate the data - adds to the inner html of an existing element (table or list)
-    divide object's times clicked by total shown
-  }
-
-*/
-
-
-
-/* TODO
-* HTML with image container and 2 img with ids
-* set left and right Item img to random
-*   - but not 2 same img at same time
-*/
-
-
 function Item(title, src) {
   this.title = title;
   this.src = src;
@@ -92,26 +21,49 @@ Item.middleTitle = document.getElementById('middle-title');
 Item.leftTitle = document.getElementById('left-title');
 Item.rightTitle = document.getElementById('right-title');
 
+var list = document.getElementById('list');
+
 // set when rendering Items
 Item.leftObject = null;
 Item.rightObject = null;
 Item.middleObject = null;
 
-
 var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 var paths = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
-
 
 for (var i = 0 ; i < names.length; i++) {
   new Item(names[i], paths[i]);
 }
 
-function renderNewItems() {
+Item.container.addEventListener('click', clickHandler);
 
-  // ensure that previous Item not shown on next round
-  // var forbidden = [Item.leftObject, Item.rightObject, Item.middleObject];
-  var forbidden = [];
+renderNewImages();
 
+/*******************************  functions  *****************************/
+function getRandomItem() {
+  var index = Math.floor(Math.random() * Item.all.length);
+  return Item.all[index];
+}
+
+function randomInRange(min, max) {
+  var range = max - min + 1; // add one since we will be flooring
+  var rand = Math.floor(Math.random() * range) + min
+  return rand;
+}
+
+function createListElement() {
+  list.innerHTML = '';
+  for (var j = 0; j < Item.all.length; j++) {
+    var li = document.createElement('li');
+    li.textContent = Item.all[j].title + ' was clicked ' + Item.all[j].clickCtr + ' and was shown ' + Item.all[j].shownCtr + ' times';
+    list.appendChild(li);
+  }
+}
+
+function renderNewImages() {
+  // ensure that previous Item not shown on next round also not in the current round
+  var forbidden = [Item.leftObject, Item.rightObject, Item.middleObject];
+  // var forbidden = [];
   do {
     Item.leftObject = getRandomItem();
   } while (forbidden.includes(Item.leftObject))
@@ -127,8 +79,6 @@ function renderNewItems() {
   } while (forbidden.includes(Item.middleObject))
   // add left Item to forbidden list so we don't double up
   forbidden.push(Item.middleObject);
-  // WARNING: if you got really unlucky the above code would result in infinite loop
-  // Can you think of safer ways?
   
   Item.leftObject.shownCtr++;
   Item.rightObject.shownCtr++;
@@ -148,115 +98,24 @@ function renderNewItems() {
   Item.leftTitle.textContent = Item.leftObject.title;
   Item.rightTitle.textContent = Item.rightObject.title;
   Item.middleTitle.textContent = Item.middleObject.title;
-
 }
-
-function getRandomItem() {
-  var index = Math.floor(Math.random() * Item.all.length);
-  return Item.all[index];
-}
-
-// not using this, just showing the better way vs. ceil
-function randomInRange(min, max) {
-  var range = max - min + 1; // add one since we will be flooring
-  var rand = Math.floor(Math.random() * range) + min
-  return rand;
-}
-
-function updateTotals() {
-
-  var tableBody = document.getElementById('report');
-
-  // Remove all children so that content doesn't get duplicated
-  // easiest way is to set innerHTML
-  // WARNING: will not remove event listeners, so be careful with that
-  tableBody.innerHTML = '';
-
-  for (var i = 0; i < Item.all.length; i++) {
-    var product = Item.all[i];
-    var row = addElement('tr', tableBody);
-    addElement('td', row, product.title);
-    addElement('td', row, '' + product.clickCtr);
-    addElement('td', row, '' + product.shownCtr);
-  }
-}
-
-function addElement(tag, container, text) {
-  var element = document.createElement(tag);
-  container.appendChild(element);
-  if(text) {
-    element.textContent = text;
-  }
-  return element;
-}
-
-function clickHandler(event) {
-
-  var clickedId = event.target.id;
-  var imgClicked;
-
-  if(clickedId === 'left-image') {
-    imgClicked = Item.leftObject;
-  } else if (clickedId === 'right-image') {
-    imgClicked = Item.rightObject;
-  } else if (clickedId === 'middle-image') {
-    imgClicked = Item.middleObject;
-  }
-
-  if(imgClicked) {
-    imgClicked.clickCtr++;
-    Item.roundCtr++;
-
-    updateTotals();
-
-    if(Item.roundCtr === Item.roundLimit) {
-
-      alert('No more clicking for you!');
-
-      Item.container.removeEventListener('click', clickHandler);
-      makeAItemChart();
-
-    } else {
-
-      renderNewItems();
-    }
-  }
-}
-
-// Notice that we're attaching event listener to the container, 
-// but event.target will allow us to which child element was actually clicked
-Item.container.addEventListener('click', clickHandler);
-
-updateTotals();
-
-renderNewItems();
-
-
 
 function makeAItemChart(){
 
   var itemNamesArray = [];
-  var itemLikesArray =[];
-  var itemShownArray =[];
+  var itemLikesArray = [];
+  var itemShownArray = [];
 
   for(var i = 0; i < Item.all.length; i++){
-    var singleItemName = Item.all[i].name;
-    itemNamesArray.push(singleItemName);
-  }
-
-  for(var i = 0; i < Item.all.length; i++){
-    var singleItemLikes = Item.all[i].clickCtr;
-    itemLikesArray.push(singleItemLikes);
-    var singleItemLikes = Item.all[i].shownCtr;
-    itemShownArray.push(singleItemLikes);
-
+    itemNamesArray.push(Item.all[i].name);
+    itemLikesArray.push(Item.all[i].clickCtr);
+    itemShownArray.push(Item.all[i].shownCtr);
   }
 
   var ctx = document.getElementById('myChart').getContext('2d');
   var ItemChart = new Chart(ctx, {
   // The type of chart we want to create
     type: 'bar',
-
     // The data for our dataset
     data: {
       labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
@@ -285,6 +144,42 @@ function makeAItemChart(){
     }
   });
 }
+
+function clickHandler(event) {
+  
+  var clickedId = event.target.id;
+  var imgClicked;
+  
+  if(clickedId === 'left-image') {imgClicked = Item.leftObject;}
+  else if (clickedId === 'right-image') {imgClicked = Item.rightObject;}
+  else if (clickedId === 'middle-image') {imgClicked = Item.middleObject;}
+  
+  if(imgClicked) {
+    imgClicked.clickCtr++;
+    Item.roundCtr++;  
+    // createListElement();
+    if(Item.roundCtr === Item.roundLimit) {      
+      alert('You already clicked 25 times!\nPress OK to display the statistics of your choices');    
+
+      Item.container.removeEventListener('click', clickHandler);
+      createListElement();
+      makeAItemChart();      
+    } else {
+      renderNewImages();
+    }
+  }
+}
+
+
+// function addElement(tag, container, text) {
+//   var element = document.createElement(tag);
+//   container.appendChild(element);
+//   if(text) {
+//     element.textContent = text;
+//   }
+//   return element;
+// }
+
 
 
 // finish
